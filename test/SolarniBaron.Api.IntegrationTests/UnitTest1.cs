dc12400fcc6378dd7ebb5986573906285a5f7728
ofcoursedude <pavel@simsa.cz>
2023-01-07 15:32:44 +0100

    Bootstrap structure (#4)

---
+++ test/SolarniBaron.Api.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace SolarniBaron.Api.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ test/SolarniBaron.Api.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace SolarniBaron.Api.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
