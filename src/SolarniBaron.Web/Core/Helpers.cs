b9da022f38359c02f528fab84ed21d3f847735d9
Pavel Simsa <pavel@simsa.cz>
2023-01-30 14:22:12 +0100

    Add web project

---
+++ src/SolarniBaron.Web/Core/Helpers.cs
@@ -0,0 +1,19 @@
+﻿namespace SolarniBaron.Web.Core;
+
+public static class Helpers
+{
+    public static string FormatMetric(decimal value, string baseUnit) => value switch
+    {
+        >= 1_000_000_000 => $"{value / 1000000000:0.##} G{baseUnit}",
+        >= 1_000_000 => $"{value / 1000000:0.##} M{baseUnit}",
+        >= 1_000 => $"{value / 1000:0.##} k{baseUnit}",
+        _ => $"{value} {baseUnit}",
+    };
+
+    public static string GetGradient(decimal current, decimal max)
+    {
+        var percent = Math.Min(current / max * 100, 100);
+        return
+            $"background-image: linear-gradient(90deg, rgb(5, 39, 103) 0%, #ffffff {percent}%); color: black; text-shadow: -1px -1px 0 #ffffff, 1px -1px 0 #ffffff, -1px 1px 0 #ffffff, 1px 1px 0 #ffffff;";
+    }
+}
---
+++ src/SolarniBaron.Web/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json.Serialization;
+using BlazorApplicationInsights;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using SolarniBaron.Domain;
+using SolarniBaron.Web;
+using SolarniBaron.Web.Core;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+var clientConfig = builder.Configuration.GetSection("ClientConfig");
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.Configure<JsonSerializerContext>(jso =>
+{
+    jso.Options.AddContext<CommonSerializationContext>();
+});
+
+builder.Services.AddSingleton<LocalStorage>();
+
+builder.Services.AddBlazorApplicationInsights();
+
+builder.Services.AddSingleton(new ClientConfig(clientConfig["LocalGetStatsUrl"] ?? "api/getstats"));
+
+builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+
+var webAssemblyHost = builder.Build();
+
+await webAssemblyHost.RunAsync();
+
+namespace SolarniBaron.Web
+{
+    public record ClientConfig(string LocalGetStatsUrl);
+}
