7601b3c8f1285df226c58164570cb2c6cd165563
Pavel Simsa <pavel@simsa.cz>
2023-01-30 15:04:23 +0100

    Add func project

---
+++ src/SolarniBaron.Func/GetStats.cs
@@ -0,0 +1,43 @@
+using System.Net;
+using System.Text.Json;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using SolarniBaron.Domain.BatteryBox.Models;
+using SolarniBaron.Domain.BatteryBox.Queries.GetStats;
+using SolarniBaron.Domain.Contracts;
+using SolarniBaron.Domain.Contracts.Queries;
+
+namespace SolarniBaron.Func
+{
+    public class GetStats
+    {
+        private readonly IQueryHandler<GetStatsQuery, GetStatsQueryResponse> _queryHandler;
+        private readonly ILogger _logger;
+
+        public GetStats(IQueryHandler<GetStatsQuery, GetStatsQueryResponse> queryHandler, ILoggerFactory loggerFactory)
+        {
+            _queryHandler = queryHandler;
+            _logger = loggerFactory.CreateLogger<GetStats>();
+        }
+
+        [Function("GetStats")]
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequestData req)
+        {
+            var loginInfo = JsonSerializer.Deserialize<LoginInfo>(req.Body);
+            var data = await _queryHandler.Get(new GetStatsQuery(loginInfo.Email, loginInfo.Password, loginInfo.UnitId));
+            if (data.ResponseStatus == ResponseStatus.Error)
+            {
+                _logger.LogError(data.Error);
+                var errorResponse = req.CreateResponse(HttpStatusCode.BadRequest);
+                errorResponse.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+                errorResponse.WriteString(data.Error);
+                return errorResponse;
+            }
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "application/json; charset=utf-8");
+            response.WriteString(JsonSerializer.Serialize(data.BatteryBoxStatus));
+            return response;
+        }
+    }
+}
