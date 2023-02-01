b43eda24181a54802da8a86f2de261b65daaa8f6
Pavel Simsa <pavel@simsa.cz>
2023-02-01 11:52:02 +0100

    Fix func

--- src/SolarniBaron.Func/Ote.cs
+++ src/SolarniBaron.Func/Ote.cs
@@ -25,9 +25,15 @@ public partial class Ote
     }
 
     [Function("ote/{date}")]
-    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req, DateOnly? date)
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req, string date)
     {
-        var result = await _queryHandler.Get(new GetPricelistQuery(date ?? DateOnly.FromDateTime(DateTime.Now)));
+        var canParse = DateOnly.TryParse(date, out var parsedDate);
+        if (!canParse)
+        {
+            var errorResponse = req.CreateResponse(HttpStatusCode.BadRequest);
+            return errorResponse;
+        }
+        var result = await _queryHandler.Get(new GetPricelistQuery(parsedDate));
 
         if (result.ResponseStatus == ResponseStatus.Error)
         {
