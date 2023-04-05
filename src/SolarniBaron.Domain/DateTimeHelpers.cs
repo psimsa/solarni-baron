c14cbf6af8082bdce3f94431bf5d398ff6774f26
psimsa <pavel@simsa.cz>
2023-04-05 07:22:10 +0000

    Fix timezone

--- src/SolarniBaron.Domain/DateTimeHelpers.cs
+++ src/SolarniBaron.Domain/DateTimeHelpers.cs
@@ -2,17 +2,11 @@
 
 public class DateTimeHelpers
 {
-    public static TimeZoneInfo GetPragueTimeZoneInfo() =>
-        Environment.OSVersion.Platform switch
-        {
-            PlatformID.Win32NT => TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time"),
-            _ => TimeZoneInfo.FindSystemTimeZoneById("Europe/Prague")
-        };
+    public static TimeZoneInfo GetPragueTimeZoneInfo() => TimeZoneInfo.FindSystemTimeZoneById("Europe/Prague");
 
     public static DateTime GetPragueDateTimeNow()
     {
-        var pragueTimeZoneInfo = GetPragueTimeZoneInfo();
-        var utcNow = DateTime.UtcNow;
-        return TimeZoneInfo.ConvertTimeFromUtc(utcNow, pragueTimeZoneInfo);
+        var pragueTimeZone = GetPragueTimeZoneInfo();
+        return TimeZoneInfo.ConvertTime(DateTimeOffset.Now, pragueTimeZone).DateTime;
     }
 }
