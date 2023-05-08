53cf85b763f9e18472e880e0ebcb9207712aa7e6
Pavel Simsa <pavel.simsa@vista.com>
2023-05-08 11:30:09 +0200

    Add build and test

--- build/Build.cs
+++ build/Build.cs
@@ -7,10 +7,13 @@ using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
 
 [GitHubActions(
     "Continuous integration",
@@ -18,7 +21,7 @@ using static Nuke.Common.IO.PathConstruction;
     OnPushBranchesIgnore = new[] { "main" },
     InvokedTargets = new[]
     {
-        nameof(Compile)
+        nameof(Compile), nameof(Test)
     },
     EnableGitHubToken = true)]
 class Build : NukeBuild
@@ -34,6 +37,9 @@ class Build : NukeBuild
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
+    [Solution(GenerateProjects = true)] readonly Solution Solution;
+
+
     Target Clean => _ => _
         .Before(Restore)
         .Executes(() =>
@@ -43,12 +49,29 @@ class Build : NukeBuild
     Target Restore => _ => _
         .Executes(() =>
         {
+            DotNetRestore(s => s
+                .SetProjectFile(Solution));
         });
 
     Target Compile => _ => _
         .DependsOn(Restore)
         .Executes(() =>
         {
+            DotNetBuild(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .SetNoRestore(InvokedTargets.Contains(Restore))
+            );
         });
 
+    Target Test => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            DotNetTest(s => s
+                           .SetProjectFile(Solution)
+                           .SetConfiguration(Configuration)
+                           .SetNoBuild(InvokedTargets.Contains(Compile))
+                       );
+        }); 
 }
